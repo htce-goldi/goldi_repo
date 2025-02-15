208663aea14b04c2643ca7d7a16b29e38fbf769d
goldi <goldikumari54htce@gmail.com>
2025-02-15 16:40:34 +0530

    Initial commit

---
+++ new repo clone/subtract.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+int firstnumber;
+int secondnumber;
+int subtract;
+printf("please enter first number:- ");
+scanf("%d",&firstnumber);
+printf("please enter secondnumber:- ");
+scanf("%d",&secondnumber);
+subtract=firstnumber-secondnumber;
+printf("subtract:- %d",subtract);
+return 0;
+}
