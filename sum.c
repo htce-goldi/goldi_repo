69fae16f2016eaa8014514f8994ebb748d4f1e5e
goldi devlopment <goldikumari54htce@gmail.com>
2025-02-18 10:58:47 +0530

    deposit _money_in_account from _user

---
+++ sum.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+
+
+    int firstnumber;
+    int secondnumber;
+    int sum;
+    printf("please enter first number:- ");
+    scanf("%d",&firstnumber);
+    printf("please enter second number:- ");
+    scanf("%d",&secondnumber);
+
+    sum=firstnumber+secondnumber;
+    printf("sum: %d",sum);
+
+
+    return 0;
+}
